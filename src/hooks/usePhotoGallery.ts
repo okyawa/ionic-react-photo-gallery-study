c521b9045a36708522af2ed6ac4a5dde8a724478
Hirokazu Okawa <h.okyawa@gmail.com>
2021-07-07 17:07:38 +0900

    Displaying Photos

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -11,16 +11,31 @@ import { Filesystem, Directory } from "@capacitor/filesystem";
 import { Storage } from "@capacitor/storage";
 import { Capacitor } from "@capacitor/core";
 
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
 export function usePhotoGallery() {
+  const [photos, setPhotos] = useState<UserPhoto[]>([]);
   const takePhoto = async() => {
     const cameraPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100,
     });
-
+    const fileName = new Date().getTime() + '.jpg';
+    const newPhotos = [
+      {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath,
+      },
+      ...photos,
+    ];
+    setPhotos(newPhotos);
   };
   return {
+    photos,
     takePhoto,
   };
 }
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -8,7 +8,7 @@ import { usePhotoGallery } from '../hooks/usePhotoGallery';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
-  const { takePhoto } = usePhotoGallery();
+  const { photos, takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -16,18 +16,21 @@ const Tab2: React.FC = () => {
           <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
           </IonFabButton>
         </IonFab>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
       </IonContent>
     </IonPage>
   );
