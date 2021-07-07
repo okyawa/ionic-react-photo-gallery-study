5729006480ccc2ab6c010510868042b63357d580
Hirokazu Okawa <h.okyawa@gmail.com>
2021-07-07 16:34:23 +0900

    タブ2を写真ギャラリーに置き換え

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,16 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+  IonCol, IonImg, IonActionSheet } from '@ionic/react';
+import { images, square, triangle, camera } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+
+function takePhoto()
+{
+
+}
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 2</IonTitle>
